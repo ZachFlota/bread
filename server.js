31b2b9c23f794ef0c57b3850a08fa4de3daafe68
ZachFlota <93958427+ZachFlota@users.noreply.github.com>
2022-04-02 10:25:38 -0400

    integrated mongoose

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 //CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 const  app = express()
 
+// DEPENDENCIES
+const methodOverride = require('method-override')
+
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
+
 
 //ROUTES
 app.get('/', (reg, res) => {
